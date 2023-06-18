e7f2692b42e2ade39374cb68858dd82095f2f260
Danil <okiidokii22@gmail.com>
2023-06-18 20:37:59 +0500

    tasks 19 and 23 are done

---
+++ C#/task19/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+int number = Convert.ToInt32(Console.ReadLine());
+void palindrome(int x){
+if (x/10000==x%10 && (x/1000)%10 == (x%100)/10){
+    Console.Write("palindrome");
+}
+else Console.Write("noPalindrome");
+}
+palindrome(number);
---
+++ C#/task23/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int cube(int x)
+{
+    return x * x * x;
+}
+
+
+for (int i = 0; i < number; i++)
+{
+    Console.Write(cube(i + 1));
+}
+
+
+
+
