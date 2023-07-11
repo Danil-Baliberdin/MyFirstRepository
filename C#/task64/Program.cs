c85fd7681b4da9705a37f555e20d9b4f4030a21b
Danil <okiidokii22@gmail.com>
2023-07-12 01:40:19 +0500

    task 64,66,68 is done

---
+++ C#/task64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// с помощью рекрсии вывести на экран числа от N до 1.
+
+
+int InputNum(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine()!);
+}
+int N = InputNum("Введите число: ");
+
+void PrintNums(int i)
+{
+    if (i == 1)
+    {
+        Console.Write(i + ". ");
+        return;
+    }
+    Console.Write(i + ", ");
+    i--;
+    PrintNums(i);
+}
+PrintNums(N);
---
+++ C#/task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// найти сумму элементов от m до n с помощью рекурсии
+
+int InputNum(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine()!);
+}
+int M = InputNum("Введите число от которого начать : ");
+int N = InputNum("Введите число от которым закончить : ");
+
+int SumNum(int m, int n)
+{
+    if (m == n)
+    {
+        return n;
+    }
+    return m + SumNum(m + 1, n);
+
+}
+int answer = SumNum(M, N);
+Console.WriteLine(answer);
